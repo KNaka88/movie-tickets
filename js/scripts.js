29e4f215308be94e6be0d0d547e73071dff34e45
David Quisenberry and Koji Nakagawa <koji@>
2017-01-24 10:44:58 -0800

    add html contents and css stylings

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+// Movie Object
+// Params: title = string, desc = string, releaseDate = string, times = string array
+function Movie (title, desc, releaseDate, times) {
+
+}
+
+// Ticket object
+// Params: movie = Movie object, age = number, time = string
+function Ticket (movie, age, time){
+
+
+}
+
+// Bill function
+function calculateBill(Ticket){
+  var totalPrice
+
+  return totalPrice;
+}
